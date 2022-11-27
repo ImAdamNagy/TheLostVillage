b3be345a845376162e9e96d978cad211dfce4108
Kinga <kiss.kinga41@gmail.com>
2022-11-27 15:17:55 +0100

    modified class and added children

--- TheLostVillage/TheLostVillage/Character.cs
+++ TheLostVillage/TheLostVillage/Character.cs
@@ -6,18 +6,17 @@ using System.Threading.Tasks;
 
 namespace TheLostVillage
 {
-    class Character
+     public class Character
     {
         public string Name { get; private set; }
         private int maxhealth;
         private int health;
-
         public int Health
         {
             get { return health; }
             set 
             { 
-                if (value <= maxhealth || value >= 0 ) {
+                if (value <= maxhealth && value >= 0 ) {
                     health = value;
                 } 
                 else if (value > maxhealth) {
@@ -25,11 +24,13 @@ namespace TheLostVillage
                 } 
                 else {
                     health = 0;
+                    IsAlive = false;
                 }
             }
         }
 
         public int Level { get; private set; }
+        public int Experience { get; private set; }
         public string Dialogue { get; private set; }
         public int Strength { get; private set; }
         public int Armor { get; private set; }
@@ -37,14 +38,20 @@ namespace TheLostVillage
 
         public Character (string name)
         {
+            IsAlive = true;
             Name = name;
             Level = 1;
+            AdjustToLevel();
+            
+            Dialogue = "I'm alive!";
+        }
+
+        public void AdjustToLevel()
+        {
             maxhealth = Level * 5;
             Health = maxhealth;
-            IsAlive = true;
             Strength = Level;
             Armor = (int)(Level / 2);
-            Dialogue = "I'm alive!";
         }
 
         public int Attack()
@@ -59,11 +66,26 @@ namespace TheLostVillage
 
         public void TakeDamage(int damage)
         {
-            Health -= damage;
-            if (Health == 0)
+            if (damage > 0)
             {
-                IsAlive = false;
+                Health -= damage;
             }
+        }
+
+        public void GainExperience(int experience)
+        {
+            Experience += experience;
+            while (Experience >= Level * 10)
+            {
+                LevelUp();
+            }
+        }
+
+        public void LevelUp()
+        {
+            Experience -= Level * 10;
+            Level++;
+            AdjustToLevel();
         }
     }
 }
---
+++ TheLostVillage/TheLostVillage/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLostVillage
+{
+    public class Player : Character
+    {
+        public Player(string name) : base(name)
+        {
+
+        }
+    }
+}
